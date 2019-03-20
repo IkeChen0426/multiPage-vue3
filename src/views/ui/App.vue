7efb1b84c7f286a58c7ac1008736b1b004aacb63
chenjl <331035263@qq.com>
2019-03-20 17:11:52 +0800

    first commit

--- src/views/ui/App.vue
+++ src/views/ui/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../../assets/logo.png">
+    <HelloWorld msg="Welcome to Your UI App"/>
+    <a href="index.html">跳转新的页面</a>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '../../components/HelloWorld.vue'
 
 export default {
   name: 'app',
